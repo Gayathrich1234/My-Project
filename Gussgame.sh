52426d03a4b6a5238acd854d9a2facf8d3455719
Gayathrich1234 <69039756+Gayathrich1234@users.noreply.github.com>
2020-07-31 21:38:13 +0530

    Update Gussgame.sh

--- Gussgame.sh
+++ Gussgame.sh
@@ -1,1 +1,25 @@
+#coded by Gayathri
 
+function guesscheck{
+        echo "Kindly guess the number of files in the current directory:"
+        read guess
+        files=$(ls -l | wc - l)
+}
+
+echo "Cordially inviting you to the Guessing Game"
+
+guesscheck
+
+while [[ $guess  -ne $files ]]
+do
+      if [[$guess -lt $files ]]
+      then
+             echo "We are sorry.Your guess is too low "
+     else        
+            echo "We are sorry. Your guess is too high."
+     fi
+     guesscheck
+done
+
+echo "Congratulation!!! It  is the correct answer."
+echo "Thank you for Participation !!!"
